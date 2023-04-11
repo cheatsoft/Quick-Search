b92c17dbae24e0307c8117abaa4beb1e4285743f
Anonymous <>
2023-04-10 23:10:43 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Quick Search
+A browser extension disguised as a safety extension that allows searching google from a popup without having to open a new window.
+
+TODO:
+- Resize google in iframe
+- Insert element into page to search instead of having to open popup
---
+++ popup.js
@@ -0,0 +1,9 @@
+document.getElementById('overlaydiv').addEventListener('click', (event) => {
+  event.target.style.display = 'none';
+});
+document.getElementById('text1').addEventListener('click', (event) => {
+  document.getElementById('overlaydiv').style.display = 'none';
+});
+document.getElementById('text2').addEventListener('click', (event) => {
+  document.getElementById('overlaydiv').style.display = 'none';
+});
