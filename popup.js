90a0926f3972d82f8d5884c68370deba7b6bf8db
Anonymous <>
2023-04-11 00:45:46 -0400

    Updated iFrame sizing

--- popup.js
+++ popup.js
@@ -1,9 +1,9 @@
 document.getElementById('overlaydiv').addEventListener('click', (event) => {
-  event.target.style.display = 'none';
+  event.target.remove();
 });
 document.getElementById('text1').addEventListener('click', (event) => {
-  document.getElementById('overlaydiv').style.display = 'none';
+  document.getElementById('overlaydiv').remove();
 });
 document.getElementById('text2').addEventListener('click', (event) => {
-  document.getElementById('overlaydiv').style.display = 'none';
+  document.getElementById('overlaydiv').remove();
 });
